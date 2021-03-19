7e93846dcd115e97c7087d0050aa8e6d28db9b93
Michael Mulet <mike@coderelay.io>
2021-03-18 20:40:32 -0700

    added time warning

--- scripts/src/fileGenerators/lib/StartNow.tsx
+++ scripts/src/fileGenerators/lib/StartNow.tsx
@@ -12,6 +12,10 @@ export default () => (
   >
     <CodeBox>
       <a href={gettingStartedLink}>Start now!</a>
+      Note: I'm still running everything myself, so I'm unavailable from 10p.m.
+      to 9a.m . If you email during the off hours, I will get back to you, but
+      not in 5 minutes like usual.
+      <a href="https://www.timeanddate.com/time/zones/cdt">CDT</a>{" "}
     </CodeBox>
   </div>
 );
